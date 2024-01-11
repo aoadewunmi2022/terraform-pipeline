91aea5a24f4875be2d0635d486aedcfe6b5fb8cc
ade <aoadewunmiwalmart@gmail.com>
2024-01-10 21:31:45 -0600

    add new t2.nano instance

--- main.tf
+++ main.tf
@@ -1,13 +1,27 @@
+# resource "aws_instance" "web" {
+#   ami                    = "ami-00b8917ae86a424c9"
+#   instance_type          = "t2.large"
+#   vpc_security_group_ids = [aws_security_group.jenkins.id]
+#   key_name               = "Ansiblekey"
+#   iam_instance_profile   = "jenkins-ec2-role"
+#   user_data              = file("./installations.sh")
+
+#   tags = {
+#     Name = "default-instance"
+#   }
+
+# }
+
 resource "aws_instance" "web" {
   ami                    = "ami-00b8917ae86a424c9"
-  instance_type          = "t2.large"
-  vpc_security_group_ids = [aws_security_group.jenkins.id]
-  key_name               = "Ansiblekey"
-  iam_instance_profile   = "jenkins-ec2-role"
-  user_data              = file("./installations.sh")
+  instance_type          = "t2.nano"
+  # vpc_security_group_ids = [aws_security_group.jenkins.id]
+  # key_name               = "Ansiblekey"
+  # iam_instance_profile   = "jenkins-ec2-role"
+  # user_data              = file("./installations.sh")
 
   tags = {
-    Name = "default-instance"
+    Name = "default-instance-2"
   }
 
 }
