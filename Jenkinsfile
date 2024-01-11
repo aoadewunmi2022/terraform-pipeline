baceb2191190ebdbf03b457247490618e6a87ae9
ade <aoadewunmiwalmart@gmail.com>
2024-01-10 22:01:23 -0600

    commented out extra instances

--- Jenkinsfile
+++ Jenkinsfile
@@ -67,13 +67,13 @@ pipeline {
             }
         }
         
-        stage('Terraform destroy') {
-            steps {
-                echo 'Terraform destroy...'                             
-                sh 'sudo terraform destroy --auto-approve'
+        // stage('Terraform destroy') {
+        //     steps {
+        //         echo 'Terraform destroy...'                             
+        //         sh 'sudo terraform destroy --auto-approve'
                
-            }
-        }
+        //     }
+        // }
         
     }
     
--- main.tf
+++ main.tf
@@ -1,41 +1,13 @@
-# resource "aws_instance" "web" {
-#   ami                    = "ami-00b8917ae86a424c9"
-#   instance_type          = "t2.large"
-#   vpc_security_group_ids = [aws_security_group.jenkins.id]
-#   key_name               = "Ansiblekey"
-#   iam_instance_profile   = "jenkins-ec2-role"
-#   user_data              = file("./installations.sh")
-
-#   tags = {
-#     Name = "default-instance"
-#   }
-
-# }
-
-# resource "aws_instance" "web" {
-#   ami                    = "ami-00b8917ae86a424c9"
-#   instance_type          = "t2.nano"
-#   # vpc_security_group_ids = [aws_security_group.jenkins.id]
-#   # key_name               = "Ansiblekey"
-#   # iam_instance_profile   = "jenkins-ec2-role"
-#   # user_data              = file("./installations.sh")
-
-#   tags = {
-#     Name = "default-instance-2"
-#   }
-
-# }
-
 resource "aws_instance" "web" {
   ami                    = "ami-00b8917ae86a424c9"
-  instance_type          = "t2.nano"
-  # vpc_security_group_ids = [aws_security_group.jenkins.id]
-  # key_name               = "Ansiblekey"
-  # iam_instance_profile   = "jenkins-ec2-role"
-  # user_data              = file("./installations.sh")
+  instance_type          = "t2.large"
+  vpc_security_group_ids = [aws_security_group.jenkins.id]
+  key_name               = "Ansiblekey"
+  iam_instance_profile   = "jenkins-ec2-role"
+  user_data              = file("./installations.sh")
 
   tags = {
-    Name = "default-instance-3"
+    Name = "default-instance"
   }
 
 }
